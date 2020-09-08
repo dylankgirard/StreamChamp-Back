cc8488ca8a6932da38902596458490da0aeba1b0
dylankgirard <dylankgirard@gmail.com>
2020-09-08 12:26:11 -0700

    Added and tested user models

--- streamchamp_back/models.py
+++ streamchamp_back/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=100)
+    favorites = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Stream(models.Model):
+    name = models.CharField(max_length=100)
+    favorited = models.ManyToManyField(User)
+
+    def __str__(self):
+        return f'Hello World {self.name} {self.favorited}'
