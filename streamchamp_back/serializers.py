a273232337fc85d1ae60501470a0cd2663429ba4
dylankgirard <dylankgirard@gmail.com>
2020-09-09 10:45:33 -0700

    Had to recreate the DB, working mostly fine now

--- streamchamp_back/serializers.py
+++ streamchamp_back/serializers.py
@@ -1,14 +1,17 @@
 from rest_framework import serializers
 from .models import User, Stream
 
+
 class UserSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = User
         fields = ('id', 'name', 'bio', 'favorites')
 
+
 class StreamSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Stream
         fields = ('id', 'name')
+        depth = 1
